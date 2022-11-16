3f7d3c5fee947c76d225bc826c94359b08be22e4
prostgles <prostgles@protonmail.com>
2022-11-16 15:00:07 +0200

    add win setup

--- win-inno-setup.ts
+++ win-inno-setup.ts
@@ -21,7 +21,7 @@ export const make = () => {
         });
     
         const buildDirFiles = fs.readdirSync(buildDir, { withFileTypes: true });
-        fs.writeFileSync(`${buildDir}\\inno.iss`, getInnoConfig(buildDirFiles, `${__dirname}\\${packedFolder}`), { encoding: "utf8" });
+        fs.writeFileSync(`${buildDir}\\inno.iss`, getInnoConfig(buildDirFiles, buildDir), { encoding: "utf8" });
         
     }
     
