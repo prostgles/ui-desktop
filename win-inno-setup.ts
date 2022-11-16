41662be764ffae08bd949476a175c5e5b45299fb
prostgles <prostgles@protonmail.com>
2022-11-16 12:27:23 +0200

    add win setup

--- win-inno-setup.ts
+++ win-inno-setup.ts
@@ -51,7 +51,7 @@ AppPublisher={#MyAppPublisher}
 AppPublisherURL={#MyAppURL}
 AppSupportURL={#MyAppURL}
 AppUpdatesURL={#MyAppURL}
-DefaultDirName={autopf}\{#MyAppName}
+DefaultDirName={autopf}\\{#MyAppName}
 ChangesAssociations=yes
 DisableProgramGroupPage=yes
 ; Uncomment the following line to run in non administrative install mode (install for current user only.)
